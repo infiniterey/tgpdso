ac2402b78fc800fbdccc43ef5fefd011e655d853
infiniterey <kevinarela@yahoo.com>
2018-07-18 19:27:59 +0800

    Rename Policy to Record menu

--- production/base/sidebarAdmin.php
+++ production/base/sidebarAdmin.php
@@ -5,8 +5,9 @@
       <li><a href="goals.php"><i class="fa fa-tasks"></i> Dashboard </a></li>
       <li><a href="home.php"><i class="fa fa-plus"></i> New Payment </a></li>
       <li><a href="add_requirements.php"><i class="fa fa-certificate"></i> Policy Requirements </a></li>
-      <li><a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-list-alt"></i> Policy <span class="fa fa-chevron-down"></span></a>
+      <li><a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-list-alt"></i> Records <span class="fa fa-chevron-down"></span></a>
         <ul class="collapse list-unstyled" id="homeSubmenu">
+          &nbsp;&nbsp;<li><a href="payment.php" style="color: white">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i style="color:red;"class="fa fa-money"></i>Policy</a></li>
           &nbsp;&nbsp;<li><a href="payment.php" style="color: white">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i style="color:red;"class="fa fa-money"></i>Payment</a></li>
           &nbsp;&nbsp;<li><a href="dueDate.php" style="color: white">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i style="color:red;" class="fa fa-calendar"></i>Due Date</a></li>
           &nbsp;&nbsp;<li><a href="SOA.php" style="color: white">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i style="color:red;" class="fa fa-cog"></i>SOA</a></li>
