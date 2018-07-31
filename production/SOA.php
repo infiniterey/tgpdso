ca82aa25080d89ce4ab2032ef26d00e97701d990
Marvel Barte <marvel.barte@gmail.com>
2018-07-31 14:21:04 +0800

    Records

--- production/SOA.php
+++ production/SOA.php
@@ -297,3 +297,34 @@ overflow: auto;
 	<!-- The Modal -->
 </body>
 </html>
+
+<script>
+
+function myFunction() {
+
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("datatable-fixed-header");
+  tr = table.getElementsByTagName("tr");
+
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+$(document).ready(function() {
+    $('#datatable-fixed-header').DataTable( {
+        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
+    } );
+} );
+
+</script>
